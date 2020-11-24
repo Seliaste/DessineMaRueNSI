a61eeac273193729c9a1c2309c47b4ffc4d8660b
mrzarf88 <monpetitleo2@gmail.com>
2020-11-24 11:40:20 +0100

    Rectangle fait

--- rectangle.py
+++ rectangle.py
@@ -6,12 +6,19 @@ def rectangle(x,y,w,h):
         x, y : coordonnées du centre de la base de rectangle
         w : largeur du rectangle
         h : hauteur du rectangle
-    Cette fonction dessine un rectangle Le point de coordonnées (x,y) est
+    Cette fonction dessine un rectangle. Le point de coordonnées (x,y) est
     sur le côté en bas au milieu
     '''
-
-    pass
-
+    turtle.goto(x-(w/2),y)
+    turtle.forward(w)
+    turtle.left(90)
+    turtle.forward(h)
+    turtle.left(90)
+    turtle.forward(w)
+    turtle.left(90)
+    turtle.forward(h)
+    turtle.left(90)
+    
 
 if __name__ == '__main__':
     rectangle(0,0,150,100)
