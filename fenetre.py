262af63eada9244f069be74745788fd23414be7c
Baltyyy <zoe.liaud88@gmail.com>
2020-11-25 08:30:25 +0100

    premières modifications fenetre

--- fenetre.py
+++ fenetre.py
@@ -1,4 +1,4 @@
-from rectangle import rectangle
+from couleur_aleatoire import couleur_aleatoire
 import turtle
 
 def fenetre(x,y):
@@ -10,10 +10,24 @@ def fenetre(x,y):
         dessine une fenetre de 30 pixels sur 30 pixels
 
     '''
-    turtle.setposition(x,y)
-    for i in range(4):
-        turtle.forward(30)
+    couleur1 = couleur_aleatoire()
+    couleur2 = couleur_aleatoire()
+    turtle.pencolor(couleur1)
+    turtle.fillcolor(couleur2)
+    turtle.penup() #on place le pinceau à la bonne position
+    turtle.setposition(x,y) 
+    turtle.left(90)  #nos coordonnées correspondent à une abscisse du centre de la fenêtre, et à une ordonnée étant le sol du niveau
+    turtle.forward(20) #on doit donc rejoindre le bord d'une fenêtre en tournant de 90° puis en avançant de la distance entre le sol et le bas de la fenêtre (j'ai mis 20 px je n'étais pas sûre vu qu'il n'y avait pas vraiment de valeur)
+    turtle.pendown() #là on peut commencer à tracer
+    turtle.begin_fill()
+    turtle.right(90) #on oriente le curseur 
+    turtle.forward(15) #on avance que de la moitié vu que notre abscisse est à la moitié de la fenêtre
+    for i in range(3):  #on trace nos trois bords consécutifs
         turtle.left(90)
+        turtle.forward(30)
+    turtle.left(90) #et on finit par tracer la première moitié que nous n'avions pas pu faire au début
+    turtle.forward(15)
+    turtle.end_fill()
 
 
 if __name__ == '__main__':
